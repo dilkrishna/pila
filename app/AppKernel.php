09dcf926b4b577b24f373e28f46d78349a7ddd49
dil <dil@crossovernepal.com>
2015-02-09 15:06:50 +0545

    updated dependencis

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
 			new UAM\Bundle\BootstrapBundle\UAMBootstrapBundle(),
             new FrontendBundle\FrontendBundle(),
+			new UAM\Bundle\StructuredDocumentBundle\UAMStructuredDocumentBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/StructuredDocumentBundle/StructuredDocumentBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace StructuredDocumentBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StructuredDocumentBundle extends Bundle
-{
-}
