59b6c36e3ef432571fbeb70b08941daa824968fe
dil <dil@crossovernepal.com>
2015-02-19 16:10:32 +0545

    create builder class

--- src/FrontendBundle/Menu/Builder.php
+++ src/FrontendBundle/Menu/Builder.php
@@ -9,8 +9,24 @@ class Builder extends ContainerAware
     public function userMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
-        $menu->addChild('Home', array('route' => 'home'));
+        $security_checker = $this->container->get('security.authorization_checker');
+
+        if ($security_checker->isGranted('ROLE_USER')) {
+            $user = $this->container->get('security.token_storage')
+                ->getToken()
+                ->getUser();
+
+            $menu->addChild('menu.user.welcome')
+                ->setExtra('translation_domain', 'editor')
+                ->setLabelAttribute('class', 'navbar-text')
+                ->setExtra('translation_params', array('%username%' => $user->getUsername()));
+
+            $menu->addChild(
+                'menu.user.logout',
+                array('route' => 'fos_user_security_logout')
+            )->setExtra('translation_domain', 'editor');
+        }
 
         return $menu;
-    }   
+    }
 }
